4921a7335a01848912fae11dbe0dbbf6ddda0c46
l-m <l-m@l-m.dev>
2025-10-28 15:37:28 +1100

    calculate GFLOPS

--- bench_RD/bench.c
+++ bench_RD/bench.c
@@ -1,9 +1,12 @@
+#define _GNU_SOURCE
 #include "bench.h"
 #include <math.h>
 #include <stdio.h>
 #include <stdint.h>
 #include <time.h>
 #include <stdbool.h>
+#include <sched.h>
+#include <unistd.h>
 
 // *Really* minimal PCG32 code / (c) 2014 M.E. O'Neill / pcg-random.org
 // Licensed under Apache License 2.0 (NO WARRANTY, etc. see website)
@@ -98,21 +101,65 @@ typedef v128_t op(v128_t, v128_t);
 # define BENCH_VOLATILE_REG(x) asm volatile("" : "+r"(x) : "r"(x) : "memory")
 # define BENCH_VOLATILE_MEM(x) asm volatile("" : "+m"(x) : "m"(x) : "memory")
 
+#include <time.h>
+#include <sys/time.h>
+double current_sec() {
+	struct timeval time;
+	if (gettimeofday(&time,NULL)){
+		return 0;
+	}
+	return (double)time.tv_sec + (double)time.tv_usec * .000001;
+}
+
+/* double measure_overhead_current_sec(){
+    double *cycles = (double *)malloc(OVERHEAD_LOOPS * sizeof(uint64_t));
+    double temp;
+    for (uint64_t i = 0; i < OVERHEAD_LOOPS; i++){
+        temp = current_sec();
+        temp = current_sec() - temp;
+        cycles[i] = temp;
+    }
+    qsort(cycles, OVERHEAD_LOOPS, sizeof(double), compare_double);
+    double output = cycles[OVERHEAD_LOOPS / 2];
+    free(cycles);
+    return output;
+} */
+
+static void pin_process_to_cpu(void){
+	cpu_set_t mask;
+
+	long num_cpus = sysconf(_SC_NPROCESSORS_ONLN);
+    if (num_cpus < 1) {
+        perror("sysconf");
+        exit(EXIT_FAILURE);
+    }
+
+	int core_id = rand() % num_cpus;
+
+	printf("sched_setaffinity(CPU %d)\n", core_id);
+
+    CPU_ZERO(&mask);
+    CPU_SET(core_id, &mask);
+
+	int result = sched_setaffinity(0, sizeof(cpu_set_t), &mask);
+    if (result == -1) {
+        perror("sched_setaffinity");
+        exit(EXIT_FAILURE);
+    }
+
+}
+
 typedef struct {
 	v128_t a;
 	v128_t b;
 } v128_pair_t;
 
-__attribute__((noinline))
-double bench(const char *name, unsigned samples, v128_pair_t sample_pairs[samples], op fn, double ref) {
-	// fprc = 1 (FE_DOWNWARD)
-	uint64_t timing_overhead = measure_overhead();
+#define UNROLL 32
 
-	v128_t c;
-	#define UNROLL 16
-
-	uint64_t temp = start_timer();
+void bench_blast(unsigned samples, v128_pair_t sample_pairs[samples], op fn) {
 	for (unsigned i = 0; i < samples / UNROLL; i += UNROLL) {
+		v128_t c;
+
 		#define LOAD_PAIR(n) v128_pair_t pair##n = sample_pairs[i + n];
 		#define THING(n) c = fn(pair##n.a, pair##n.b); BENCH_VOLATILE_MEM(c); BENCH_CLOBBER();
 		
@@ -120,28 +167,46 @@ double bench(const char *name, unsigned samples, v128_pair_t sample_pairs[sample
 		LOAD_PAIR(4)  LOAD_PAIR(5)  LOAD_PAIR(6)  LOAD_PAIR(7)
 		LOAD_PAIR(8)  LOAD_PAIR(9)  LOAD_PAIR(10) LOAD_PAIR(11)
 		LOAD_PAIR(12) LOAD_PAIR(13) LOAD_PAIR(14) LOAD_PAIR(15)
+		LOAD_PAIR(16)  LOAD_PAIR(17)  LOAD_PAIR(18)  LOAD_PAIR(19)
+		LOAD_PAIR(20)  LOAD_PAIR(21)  LOAD_PAIR(22)  LOAD_PAIR(23)
+		LOAD_PAIR(24)  LOAD_PAIR(25)  LOAD_PAIR(26) LOAD_PAIR(27)
+		LOAD_PAIR(28) LOAD_PAIR(29) LOAD_PAIR(30) LOAD_PAIR(31)
 
 		THING(0)  THING(1)  THING(2)  THING(3)
 		THING(4)  THING(5)  THING(6)  THING(7)
 		THING(8)  THING(9)  THING(10) THING(11)
 		THING(12) THING(13) THING(14) THING(15)
+		THING(16)  THING(17)  THING(18)  THING(19)
+		THING(20)  THING(21)  THING(22)  THING(23)
+		THING(24)  THING(25)  THING(26) THING(27)
+		THING(28) THING(29) THING(30) THING(31)
 	}
-	temp = end_timer() - temp;
+}
 
-	double ticks = (temp - timing_overhead);
-	ticks /= (double)samples;
+
+__attribute__((noinline))
+double bench(const char *name, unsigned samples, v128_pair_t sample_pairs[samples], op fn, double ref) {
+	bench_blast(samples, sample_pairs, fn);
+	bench_blast(samples, sample_pairs, fn);
+	bench_blast(samples, sample_pairs, fn);
+	bench_blast(samples, sample_pairs, fn);
+	bench_blast(samples, sample_pairs, fn);
+	bench_blast(samples, sample_pairs, fn);
+	bench_blast(samples, sample_pairs, fn);
 	
-	BENCH_VOLATILE_MEM(c);
+	double temp = current_sec();
+	bench_blast(samples, sample_pairs, fn);
+	temp = current_sec() - temp;
 
+	double gflops = (double)samples / temp / 1e9;
+	
 	if (isnan(ref)) {
-		printf("  fprc(1): %8.2f ticks \"%s\"\n", ticks, name);	
+		printf("  fprc(1): %8.2f GFLOPS \"%s\"\n", gflops, name);	
 	} else {
-		double ref_overhead = ticks / ref;
-		printf("  fprc(1): %8.2f ticks \"%s\" (x%.2f overhead)\n", ticks, name, ref_overhead);
+		double ref_overhead = ref / gflops;
+		printf("  fprc(1): %8.2f GFLOPS \"%s\" (x%.2f overhead)\n", gflops, name, ref_overhead);
 	}
-
-#undef UNROLL
-	return ticks;
+	return gflops;
 }
 
 __attribute__((noinline))
@@ -154,7 +219,7 @@ v128_t hard_fmul_1(v128_t dest, v128_t src) {
 	return wasm_f64x2_mul(dest, src);
 }
 
-#define SAMPLES 160000
+#define SAMPLES 320000
 
 v128_pair_t sample_pairs[SAMPLES];
 
@@ -179,6 +244,10 @@ void bench_fill(double lo, double hi, bool is_F) {
 int main(void) {
 	// F (1e+4, 1e+14) and (-1e+14, -1e+4)
 	// E {0} and (1e-40, inf)
+
+	srand(time(NULL));
+	pin_process_to_cpu();
+	srand(1337);
 
 	bench_fill(1e+4, 1e+14, true);
 
--- bench_RD/timer.h
+++ bench_RD/timer.h
@@ -43,6 +43,14 @@ uint64_t end_timer(void)
    return ((uint64_t)a | ((uint64_t)d << 32));
 }
 
+int compare_double(const void *x, const void *y)
+{
+  double xx = *(double*)x, yy = *(double*)y;
+  if (xx < yy) return -1;
+  if (xx > yy) return  1;
+  return 0;
+}
+
 int compare_u64(const void *x, const void *y)
 {
   uint64_t xx = *(uint64_t*)x, yy = *(uint64_t*)y;
